2f229e503756017336dea4c2013e4b2af6c1bf0c
Anat-74 <114754477+Anat-74@users.noreply.github.com>
2023-11-22 17:36:46 +0300

    hyrthjfghfdghfghfghfghgf

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,26 +3,106 @@ import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js"  ;
 
-/*
-const rowIcons = document.querySelector('.row-icons');
-const rowIconsItemsBtn = document.querySelectorAll('.row-icons__items-btn');
 
-if(rowIcons) {
-rowIcons.addEventListener('click', (e) => {
-   const target = e.target;
 
-     if(target.classList.contains('row-icons__items-btn')); {
+window.addEventListener("click", function(e){
+   const rowItemsBtn = document.querySelectorAll('.row-icons__items-btn')
+   const imageItems = document.querySelectorAll('.columns-image__items')
+   const load = document.querySelector('.portfolio__load')
 
-      rowIconsItemsBtn.forEach(el => {
-      el.classList.remove('row-icons__items-btn--active')
-   });
+   if(e.target.classList.contains('row-icons__items-btn')) {
 
-      target.classList.add('row-icons__items-btn--active');
-      rowIcons.classList.remove('row-icons__items-btn--active');
+      rowItemsBtn.forEach((el) => {
+         el.classList.remove('row-icons__items-btn--active')
+      })
+
+      e.target.classList.toggle('row-icons__items-btn--active')
+
+      if(e.target.dataset.tab==='tab-1') {
+         imageItems.forEach((el) => {
+            el.classList.remove('none')
+        })
+   
+      } else {
+         imageItems.forEach((el) => {
+            el.classList.add('none')
+        })
+       }
+      const contentTab = document.querySelector('#' + e.target.dataset.tab)
+      contentTab.classList.remove('none')
    }
-   });
-}
+
+   if(e.target.classList.contains('portfolio__btn')) {
+
+      imageItems.forEach((el) => {
+         el.classList.remove('none')
+     })
+
+     rowItemsBtn.forEach((el) => {
+      el.classList.remove('row-icons__items-btn--active')
+     })
+
+     const activeBtn = document.querySelector('#active')
+     activeBtn.classList.add('row-icons__items-btn--active')
+   }
+
+})
+
+/*
+const itemsBtn = document.querySelectorAll('[data-tab]')
+const imageItems = document.querySelectorAll('.columns-image__items')
+const load = document.querySelector('.portfolio__load')
+
+itemsBtn.forEach((item) => {
+   item.addEventListener('click', function() {
+      if(this.classList.contains('row-icons__items-btn')) {
+         itemsBtn.forEach((el) => {
+            el.classList.remove('row-icons__items-btn--active')
+         })
+         this.classList.toggle('row-icons__items-btn--active')
+      }
+
+
+     if(this.dataset.tab === 'tab-1') {
+      imageItems.forEach((el) => {
+         el.classList.remove('none')
+      })
+   }else {
+      imageItems.forEach((el) => {
+         el.classList.add('none')
+        })
+   }
+   
+   const contentTab = document.querySelector('#' + this.dataset.tab)
+   contentTab.classList.remove('none')
+
+   })
+})
 */
+
+
+
+
+
+
+function removeClasses(elements, className) {
+   elements.forEach(el => {
+      el.classList.remove(className)
+   })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
